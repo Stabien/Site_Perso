96c6000f8fe9a98fd835c397292ed6729b8dc2a1
Stabien <bastien.piedallu@gmail.com>
2020-08-03 17:48:17 +0200

    navbar construction

---
+++ header.php
@@ -0,0 +1,17 @@
+<header class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="#" class="navbar-brand">Bastien PIEDALLU</a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-content">
+       <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar-content">
+        <nav>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">Présentation</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Mes compétences</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Mon expérience</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Mes projets</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Me contacter</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
