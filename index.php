a78b1db1dd00f71d5b15bac6aa4e3ac405d3422f
Stabien <bastien.piedallu@gmail.com>
2020-08-08 19:44:22 +0200

    header nav and presentation globally finished || skills in progress

--- index.php
+++ index.php
@@ -3,14 +3,141 @@
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <title>Template</title>
 </head>
 
 <body>
     <?php include('header.php'); ?>
+    <div id="banner">
+        <img src="images/banniere.png">
+        <div id="titles">
+            <h1>Bastien Piedallu</h1>
+            <h2>DÉVELOPPEUR WEB JUNIOR</h2>
+        </div>
+    </div>
+    <div id="profil">
+        <img src="images/profil.jpg">
+    </div>
+    <main class="container-fluid">
+        <div id="presentation_row" class="row">
+            <div id="presentation" class="col-8 offset-2" data-aos="fade-up" data-aos-duration="1500">
+                <h2>Présentation</h2>
+                <p>Cela fait maintenant plusieurs années
+                que je suis passionné par le développement informatique.
+                Je me trouve actuellement en BTS SIO (option SLAM) et je suis à la recherche
+                d'une alternance en tant que développeur web dans le cadre de ma deuxième année.
+                </p>
+            </div>
+        </div>
+        <h2 id="competences_title">Compétences<h2>
+        <div id="competences" class="container">
+            <div class="row">
+                <div class="col-lg">
+                    <h3>Développement Web</h3>
+                    <ul>
+                        <li>HTML & CSS
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>JavaScript
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>PHP & MySQL
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg">
+                    <h3>Langages de programmation</h3>
+                    <ul>
+                        <li>C
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>C++
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>Python
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                    <h3>Frameworks</h3>
+                    <ul>
+                        <li><span>jQuery</span>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>React
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>Symfony
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="40"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg">
+                    <h3>Langues</h3>
+                    <ul>
+                        <li>Français
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>Anglais
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                        <li>Espagnol
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script>
+    AOS.init();
+    </script>
 </body>
 </html>
