bf03d378bb81f1cf0875251262d0d238b5a2c7e1
Stabien <bastien.piedallu@gmail.com>
2020-08-12 14:15:16 +0200

    aos bug fixed || competence animation finished

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@3.0.0-beta.6/dist/aos.css"/>
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <title>Template</title>
 </head>
 
@@ -36,7 +35,7 @@
                 </p>
             </div>
         </div>
-        <div id="animation-container" data-aos="fade-up" data-aos-duration="1000">
+        <div id="animation-container" data-aos="fade-up" data-aos-duration="1000" data-aos-id="competences">
             <h2 id="competences_title">Compétences<h2>
             <div id="competences" class="container">
                 <div class="row">
@@ -138,16 +137,16 @@
             </div>
         </div>
     </main>
-    <script>
-    $(document).on("scroll", function() {
-        $(".progress-bar").css("width", function() {
-            return $(this).attr("aria-valuenow") + "%";
-        });
-    });
-    </script>
+    <script src="https://unpkg.com/aos@3.0.0-beta.6/dist/aos.js"></script>
     <script>
     AOS.init({
         once: true
+    });
+
+    $(document).on("aos:in:competences", function() {
+        $(".progress-bar").css("width", function() {
+            return $(this).attr("aria-valuenow") + "%";
+        });
     });
     </script>
 </body>
