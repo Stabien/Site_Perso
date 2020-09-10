8d29f143b59ad1d06f5724768aee66dd5b627559
Stabien <bastien.piedallu@gmail.com>
2020-09-11 01:33:48 +0200

    updating viewport parameters + title changed from link to span

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=400px, initial-scale=1 maximum-scale=1, minimum-scale=0.7">
+    <meta name="viewport" content="width=600, maximum-scale=1">
     <title>Template</title>
 </head>
 
 <body>
     <header class="navbar navbar-expand-lg position-fixed">
-        <a href="#" class="navbar-brand">Bastien PIEDALLU</a>
+        <span href="#" class="navbar-brand">Bastien PIEDALLU</span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
            <span class="navbar-toggler-icon"></span>
         </button>
